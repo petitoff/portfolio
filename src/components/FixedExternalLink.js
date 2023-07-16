8af2e9180bbd93c8f5587911379c174f6c369b79
Błażej Domagała <petitoff@outlook.com>
2023-07-16 19:45:15 +0200

    deleted commented code with unused import

--- src/components/FixedExternalLink.js
+++ src/components/FixedExternalLink.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaGithub, FaGithubAlt, FaLinkedinIn } from "react-icons/fa";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import styled from "styled-components";
 import { link } from "../styles/mixins";
 
@@ -7,17 +7,10 @@ const StyledSideElement = styled.div`
   width: 40px;
   position: fixed;
   bottom: 0;
-  /* left: ${(props) => (props.orientation === "left" ? "40px" : "auto")}; */
-  /* right: ${(props) => (props.orientation === "left" ? "auto" : "40px")}; */
   left: 30px;
   right: auto;
   z-index: 10;
   color: var(--light-slate);
-
-  @media (max-width: 1080px) {
-    /* left: ${(props) => (props.orientation === "left" ? "20px" : "auto")}; */
-    /* right: ${(props) => (props.orientation === "left" ? "auto" : "20px")}; */
-  }
 
   @media (max-width: 768px) {
     display: none;
