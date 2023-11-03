cad81171c1363c44e329ec4e85e904e462a46930
Błażej Domagała <petitoff@outlook.com>
2023-11-03 10:03:29 +0100

    WIN/ new nextjs implementation, two first section is complete

---
+++ lib/data.ts
@@ -0,0 +1,88 @@
+import React from 'react';
+import { CgWorkAlt } from 'react-icons/cg';
+import { FaReact } from 'react-icons/fa';
+import { LuGraduationCap } from 'react-icons/lu';
+import corpcommentImg from '@/public/corpcomment.png';
+import rmtdevImg from '@/public/rmtdev.png';
+import wordanalyticsImg from '@/public/wordanalytics.png';
+
+export const links = [
+  {
+    name: 'Home',
+    hash: '#home',
+  },
+  {
+    name: 'About',
+    hash: '#about',
+  },
+  {
+    name: 'Projects',
+    hash: '#projects',
+  },
+  {
+    name: 'Skills',
+    hash: '#skills',
+  },
+  {
+    name: 'Experience',
+    hash: '#experience',
+  },
+  {
+    name: 'Contact',
+    hash: '#contact',
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: 'Graduated bootcamp',
+    location: 'Miami, FL',
+    description:
+      'I graduated after 6 months of studying. I immediately found a job as a front-end developer.',
+    icon: React.createElement(LuGraduationCap),
+    date: '2019',
+  },
+  {
+    title: 'Front-End Developer',
+    location: 'Orlando, FL',
+    description:
+      'I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.',
+    icon: React.createElement(CgWorkAlt),
+    date: '2019 - 2021',
+  },
+  {
+    title: 'Full-Stack Developer',
+    location: 'Houston, TX',
+    description:
+      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+    icon: React.createElement(FaReact),
+    date: '2021 - present',
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: 'Dev Docket',
+    description:
+      'I created full-stack application to manage projects. App have a lot o feature for managing projects and teams.',
+    tags: ['React & Vite', 'Nestjs', 'Postgresql', 'Tailwind', 'REST API'],
+    imageUrl: corpcommentImg,
+  },
+] as const;
+
+export const skillsData = [
+  'HTML',
+  'CSS',
+  'Tailwind',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Node.js',
+  'Nest.js',
+  'Git',
+  'Redux Toolkit',
+  'Express',
+  'PostgreSQL',
+  'Python',
+  'Framer Motion',
+] as const;
