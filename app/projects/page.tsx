dfc3a556ae88efe181555fd8b3c96ef7d8ac4a03
Błażej Domagała <petitoff@outlook.com>
2024-02-16 19:39:58 +0100

    Add Suspense to Project component

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,12 @@
 import Project from "@/components/projects/project";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
     <div className="flex flex-col items-center px-4">
-      <Project />
+      <Suspense>
+        <Project />
+      </Suspense>
     </div>
   );
 }
