96aca088e941fc7ee8399e4f247e213da2151537
Błażej Domagała <petitoff@outlook.com>
2024-02-18 23:03:07 +0100

    Add scroll animation to Experience component

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import SectionHeading from "./section-heading";
 import {
   VerticalTimeline,
@@ -8,52 +8,62 @@ import {
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from "@/lib/data";
-import { useSectionInView } from "@/lib/hooks";
 import { useTheme } from "@/context/theme-context";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 export default function Experience() {
-  const { ref } = useSectionInView("Experience");
   const { theme } = useTheme();
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.33 1"],
+  });
+
+  const opocityProgress = useTransform(scrollYProgress, [0.2, 0.5], [0, 1]);
 
   return (
     <section id="experience" ref={ref} className="mb-28 scroll-mt-28 sm:mb-40">
-      <SectionHeading>My experience</SectionHeading>
-      <VerticalTimeline lineColor="">
-        {experiencesData.map((item, index) => (
-          <React.Fragment key={index}>
-            <VerticalTimelineElement
-              visible
-              contentStyle={{
-                background:
-                  theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
-                boxShadow: "none",
-                border: "1px solid rgba(0, 0, 0, 0.05)",
-                textAlign: "left",
-                padding: "1.3rem 2rem",
-              }}
-              contentArrowStyle={{
-                borderRight:
-                  theme === "light"
-                    ? "0.4rem solid #9ca3af"
-                    : "0.4rem solid rgba(255, 255, 255, 0.5)",
-              }}
-              date={item.date}
-              icon={item.icon}
-              iconStyle={{
-                background:
-                  theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
-                fontSize: "1.5rem",
-              }}
-            >
-              <h3 className="font-semibold capitalize">{item.title}</h3>
-              <p className="!mt-0 font-normal">{item.location}</p>
-              <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
-                {item.description}
-              </p>
-            </VerticalTimelineElement>
-          </React.Fragment>
-        ))}
-      </VerticalTimeline>
+      <motion.div ref={ref} style={{ opacity: opocityProgress }}>
+        <SectionHeading>My experience</SectionHeading>
+        <VerticalTimeline lineColor="" animate>
+          {experiencesData.map((item, index) => (
+            <React.Fragment key={index}>
+              <VerticalTimelineElement
+                visible
+                contentStyle={{
+                  background:
+                    theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
+                  boxShadow: "none",
+                  border: "1px solid rgba(0, 0, 0, 0.05)",
+                  textAlign: "left",
+                  padding: "1.3rem 2rem",
+                }}
+                contentArrowStyle={{
+                  borderRight:
+                    theme === "light"
+                      ? "0.4rem solid #9ca3af"
+                      : "0.4rem solid rgba(255, 255, 255, 0.5)",
+                }}
+                date={item.date}
+                icon={item.icon}
+                iconStyle={{
+                  background:
+                    theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
+                  fontSize: "1.5rem",
+                }}
+              >
+                <h3 className="font-semibold capitalize">{item.title}</h3>
+                <p className="!mt-0 font-normal">{item.location}</p>
+                <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
+                  {item.description}
+                </p>
+              </VerticalTimelineElement>
+            </React.Fragment>
+          ))}
+        </VerticalTimeline>
+      </motion.div>
     </section>
   );
 }
