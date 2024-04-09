daa3a977b139fbbfd9c526ac8080cccf67e8f889
Błażej Domagała <petitoff@outlook.com>
2024-04-09 20:41:45 +0200

    Update navigation styling in Header component

--- components/header.tsx
+++ components/header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
 
-      <nav className="fixed left-1/2 top-[0.15rem] flex h-12 -translate-x-1/2 py-2 max-sm:w-full sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+      <nav className="fixed left-1/2 top-[0.15rem] flex h-12 -translate-x-1/2 py-1 max-sm:w-full sm:top-[1.7rem] sm:h-[initial] sm:py-0">
         <ul className="flex w-full flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {links.map((link) => (
             <motion.li
