f2d4ee6673ccf67b9bfd189d1143b36196a9c5dc
Błażej Domagała <petitoff@outlook.com>
2024-04-09 22:12:21 +0200

    Refactor Google Analytics initialization in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect } from "react";
 import About from "@/components/about";
 import Experience from "@/components/experience";
 import Header from "@/components/header";
@@ -7,10 +8,38 @@ import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
-import { useGoogleAnalytics } from "@/hooks/use-google-analytics";
+import Analytics from "react-ga4";
+import router from "next/router";
 
 export default function Home() {
-  useGoogleAnalytics();
+  useEffect(() => {
+    if (
+      typeof window !== "undefined" &&
+      process.env.NODE_ENV === "production"
+    ) {
+      const gaId = process.env.NEXT_PUBLIC_GA_ID;
+
+      if (gaId) {
+        Analytics.initialize(gaId);
+        const handleRouteChange = (url: string) => {
+          Analytics.send({ hitType: "pageview", page: url });
+        };
+
+        // Śledzenie pierwszego załadowania strony
+        Analytics.send({ hitType: "pageview", page: window.location.pathname });
+
+        // Dodanie obserwatora dla zmian ścieżki
+        router.events.on("routeChangeComplete", handleRouteChange);
+
+        // Oczyszczenie event listenera
+        return () => {
+          router.events.off("routeChangeComplete", handleRouteChange);
+        };
+      } else {
+        console.warn("Google Analytics ID is not set");
+      }
+    }
+  }, []);
 
   return (
     <main className="flex flex-col items-center px-4">
