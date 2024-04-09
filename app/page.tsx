52efb16674b2168a6ea38a3cf48be0cc01702ad7
Błażej Domagała <petitoff@outlook.com>
2024-04-09 20:50:44 +0200

    Add Google Analytics initialization to Home component

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,18 @@ import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
+import { useEffect } from "react";
+import Analytics from "react-ga4";
 
 export default function Home() {
+  useEffect(() => {
+    const gaId = process.env.NEXT_PUBLIC_GA_ID;
+
+    if (!gaId) return console.error("Google Analytics ID is not set");
+
+    Analytics.initialize(gaId);
+  }, []);
+
   return (
     <main className="flex flex-col items-center px-4">
       <Header />
